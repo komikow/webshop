f29de792391c2e1363545308580fe428bd0750b4
Komikov Sergey <komikovsergey@gmail.com>
2023-02-14 19:46:08 +0300

    add fixed error code by CreateUserController

---
+++ src/main/java/by/it/academy/controllers/SelectUserController.java
@@ -0,0 +1,7 @@
+package by.it.academy.controllers;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+@WebServlet
+public class SelectUserController extends HttpServlet {
+}
